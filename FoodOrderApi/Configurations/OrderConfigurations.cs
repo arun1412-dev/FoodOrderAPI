a15de6d098591e50e057805d8f990fd3b8c49510
Sathika Jaheer Hussain <sathika.jaheer@solitontech.local>
2023-05-16 12:32:58 +0530

    feat: Add the base code

---
+++ FoodOrderApi/Configurations/OrderConfigurations.cs
@@ -0,0 +1,15 @@
+﻿using FoodOrderApi.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FoodOrderApi.Configurations
+{
+    public class OrderConfigurations : IEntityTypeConfiguration<Order>
+    {
+        public void Configure(EntityTypeBuilder<Order> builder)
+        {
+            builder.Property(x => x.ProductID).HasColumnName("ProductID (Numbers)");
+            builder.Property(x => x.CustomerName);
+        }
+    }
+}
---
+++ FoodOrderApi/Repository/FoodApiDbContext.cs
@@ -0,0 +1,22 @@
+﻿using FoodOrderApi.Model;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace FoodOrderApi.Repository
+{
+    public class FoodApiDbContext : DbContext
+    {
+        public FoodApiDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Restaurant> Restaurants { get; set; }
+        public DbSet<Menu> Menus { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
